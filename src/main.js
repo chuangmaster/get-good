8c5c33e05e47a64815660cac63c7df5631518c3a
chuangmaster <chuang.master@gmail.com>
2023-09-04 01:54:44 +0800

    加入 font awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import router from "./router/index.js";
 import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+// import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
+
+
+
+library.add(faTrashCan)
+// library.add(faTrashCan)
+
+
+
 // import Vuex from 'vuex'
 // const store = new Vuex.Store({
 //   state: {
@@ -20,4 +37,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //     }
 //   },
 // });
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store)
+.use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount("#app");
